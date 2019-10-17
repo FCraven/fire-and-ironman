669d44de588ccc8da08b1aaafe9c9cfc0a9ed1e9
FCraven <craven.francis@gmail.com>
2019-10-17 13:36:31 -0400

    500 handler added

--- server/server.js
+++ server/server.js
@@ -5,16 +5,27 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const apiRoutes = require('./apiRoutes')
 
+// Logging Middleware
 app.use(morgan('dev'))
 
+// Static Serving Middleware
 app.use(express.static(path.join(__dirname, '../public')))
 
+//Body Parsing Middleware
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// API Routes
 app.use('/api', apiRoutes)
 
+//If no routes match, send index.html
 app.get('*', function (req,res) {
   res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+//500
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || ' Internal Server Error')
 })
