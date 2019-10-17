2ccf47d5ee2313eb083edc15d609d7b3c74f7a5c
FCraven <craven.francis@gmail.com>
2019-10-17 13:30:12 -0400

    server * route serves index.html

--- server/server.js
+++ server/server.js
@@ -14,3 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/api', apiRoutes)
+
+app.get('*', function (req,res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+})
