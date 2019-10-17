ea91a8bf6ebdfd224677529ef45e51402f1268c2
FCraven <craven.francis@gmail.com>
2019-10-17 13:26:44 -0400

    error handler added to api routes

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -3,4 +3,11 @@ const router = require('express').Router();
   //  /api/users
 router.use('/users', require('./users'))
 
+//404 Error Handler
+router.use(function (req, res, next) {
+  const err = new Error('Not found.');
+  err.status = 404;
+  next(err);
+});
+
 module.exports = router;
