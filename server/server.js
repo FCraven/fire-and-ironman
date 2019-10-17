1b28adfd4190e4681b5c42726b7e6ef1760066e3
FCraven <craven.francis@gmail.com>
2019-10-17 13:13:12 -0400

    added body parsing middleware

--- server/server.js
+++ server/server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express()
 const path = require('path')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 app.use(morgan('dev'))
 
-app.use(express.static(path.join(__dirname, '../public/assets')))
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
