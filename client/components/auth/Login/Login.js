a5b5b065d2d7cd550f48d602785014730987f194
FCraven <craven.francis@gmail.com>
2019-10-30 11:49:52 -0400

    added shell Login

---
+++ client/components/auth/Login/Login.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Hellogin World
+      </div>
+    )
+  }
+}
+
